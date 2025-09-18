c07f7421019281d09fae0f37bcd769920c4ec695
arey-dev <raborja.dev@gmail.com>
2025-09-19 02:06:10 +0800

    draw player + movement

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
 
+from player import *
+
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+ 
+    Player.containers = (updatables, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +24,15 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        
+        for drawable in drawables:
+            drawable.draw(screen)
+
+        updatables.update(dt)
+
         pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
